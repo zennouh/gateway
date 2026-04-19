6beb53e4c32482e172bac1436abf168718e39ec3
nouh <nouh.nowile@gmail.com>
2026-04-19 13:30:11 +0100

    remove unuse fields for the Admin entity

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -8,18 +8,5 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: AdminRepository::class)]
 class Admin extends User
 {
-    #[ORM\Column]
-    private string $department;
-
-    public function getDepartment(): string
-    {
-        return $this->department;
-    }
-
-    public function setDepartment(string $department): static
-    {
-        $this->department = $department;
-
-        return $this;
-    }
+   
 }
